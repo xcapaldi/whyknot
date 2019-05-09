e99f54da56bcd9c8041aad594ab848c987792e39
xcapaldi <xcapaldi@scribo.biz>
2019-05-09 16:24:51 -0400

    Start working in tkinter instead of pygame.

--- whyknot.py
+++ whyknot.py
@@ -1,17 +1,14 @@
-import sys
-import pygame as pg
+import tkinter as tk
+import tkinter.filedialog as fd
 
-pg.init()
+root = tk.Tk()
+w = tk.Canvas(root, width=200, height=200)
+w.pack()
+w.create_line(0,0,200,100)
+w.create_line(0,100,200,0,fill="red",dash=(4,4))
+w.create_rectangle(50,25,150,75,fill="blue")
 
-width = 320
-height = 240
-background = [0, 0, 0]
+root.filename = fd.asksaveasfilename(initialdir = "/",title="Select File")
+print(root.filename)
 
-screen = pg.display.set_mode((width, height))
-
-while 1:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            sys.exit()
-    screen.fill(background)
-    pg.display.flip()
+root.mainloop()
