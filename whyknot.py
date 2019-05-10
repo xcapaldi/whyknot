6eb5b9667086f4f3ee9df91bf282c6f0d2684431
xcapaldi <xcapaldi@scribo.biz>
2019-05-10 14:33:11 -0400

    Run black and pipreqs.

--- whyknot.py
+++ whyknot.py
@@ -10,11 +10,11 @@ import tkinter.filedialog as fd
 root = tk.Tk()
 w = tk.Canvas(root, width=200, height=200)
 w.pack()
-w.create_line(0,0,200,100)
-w.create_line(0,100,200,0,fill="red",dash=(4,4))
-w.create_rectangle(50,25,150,75,fill="blue")
+w.create_line(0, 0, 200, 100)
+w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
+w.create_rectangle(50, 25, 150, 75, fill="blue")
 
-root.filename = fd.asksaveasfilename(initialdir = "/",title="Select File")
+root.filename = fd.asksaveasfilename(initialdir="/", title="Select File")
 print(root.filename)
 
 root.mainloop()
